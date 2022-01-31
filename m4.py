04dc1dcaf7495e61d992ed7342cdb4d5f8b2e4db
Laxmi Yadav <91363406+yadavlaxmi@users.noreply.github.com>
2022-01-31 19:21:08 +0530

    Add files via upload

---
+++ m4.py
@@ -0,0 +1,10 @@
+places=["delhi", "gujrat", "rajasthan", "punjab", "kerala"]
+i=1
+while i<len(places):
+  b=(places[i-1])
+  i=i-1
+  print(b)
+#   i=0
+# while i<len(places):
+#   i=i-1
+#   print(places[i-1])
